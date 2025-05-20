da62f8c268f8f06f1dec6528b165240f59ed5af6
Padmanabansals <padmanabansals.net@gmail.com>
2025-05-20 13:28:51 +0530

    added

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://tiny-magic-1.onrender.com"]
+}));
+
 app.use(express.json());
 
 // Chat history folder
